3a07dea98766da3433b002fd39aa67d5c86b8908
oluwayemidev <lordryefox@gmail.com>
2025-06-17 10:00:09 +0100

    build upload

---
+++ build/assets/index-BU6RKJ7a.js
@@ -0,0 +1,1 @@
+import{g as r,f as t}from"./app-CYstcUKF.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ build/assets/nav-user-DliDTf62.js
@@ -0,0 +1,21 @@
+import{m as x,j as e}from"./app-CYstcUKF.js";import{q as h,r as p,s as j,t as g,v as u,w as N,g as r,x as d,i as l,y as v,z as y,B as M,E as n,G as c,H as s,L as f}from"./dropdown-menu-DtU-1iCl.js";import{c as t}from"./createLucideIcon-aYcdhqyH.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=[["path",{d:"M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z",key:"3c2336"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],k=t("BadgeCheck",w);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=[["path",{d:"M10.268 21a2 2 0 0 0 3.464 0",key:"vwvbt9"}],["path",{d:"M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326",key:"11g9vi"}]],_=t("Bell",b);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const A=[["rect",{width:"20",height:"14",x:"2",y:"5",rx:"2",key:"ynyp8z"}],["line",{x1:"2",x2:"22",y1:"10",y2:"10",key:"1b3vmo"}]],C=t("CreditCard",A);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=[["path",{d:"M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z",key:"4pj2yx"}],["path",{d:"M20 3v4",key:"1olli1"}],["path",{d:"M22 5h-4",key:"1gvqau"}],["path",{d:"M4 17v2",key:"vumght"}],["path",{d:"M5 18H3",key:"zchphs"}]],S=t("Sparkles",L);function z({user:a}){const{isMobile:o}=h(),{post:i}=x(),m=()=>{i(route("admin.logout"))};return e.jsx(p,{children:e.jsx(j,{children:e.jsxs(g,{children:[e.jsx(u,{asChild:!0,children:e.jsxs(N,{size:"lg",className:"data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground",children:[e.jsxs(r,{className:"h-8 w-8 rounded-lg",children:[e.jsx(d,{src:a.avatar,alt:a.name}),e.jsx(l,{className:"rounded-lg",children:a.name.charAt(0)})]}),e.jsxs("div",{className:"grid flex-1 text-left text-sm leading-tight",children:[e.jsx("span",{className:"truncate font-medium",children:a.name}),e.jsx("span",{className:"truncate text-xs",children:a.email})]}),e.jsx(v,{className:"ml-auto size-4"})]})}),e.jsxs(y,{className:"min-w-56 rounded-lg",side:o?"bottom":"right",align:"end",sideOffset:4,children:[e.jsx(M,{className:"p-0 font-normal",children:e.jsxs("div",{className:"flex items-center gap-2 px-3 py-2 text-left text-sm",children:[e.jsxs(r,{className:"h-8 w-8 rounded-lg",children:[e.jsx(d,{src:a.avatar,alt:a.name}),e.jsx(l,{className:"rounded-lg",children:a.name.charAt(0)})]}),e.jsxs("div",{className:"grid flex-1 leading-tight",children:[e.jsx("span",{className:"font-medium",children:a.name}),e.jsx("span",{className:"text-xs text-muted-foreground",children:a.email})]})]})}),e.jsx(n,{}),e.jsx(c,{children:e.jsxs(s,{children:[e.jsx(S,{className:"mr-2 h-4 w-4"}),"Upgrade to Pro"]})}),e.jsx(n,{}),e.jsxs(c,{children:[e.jsxs(s,{children:[e.jsx(k,{className:"mr-2 h-4 w-4"}),"Account"]}),e.jsxs(s,{children:[e.jsx(C,{className:"mr-2 h-4 w-4"}),"Billing"]}),e.jsxs(s,{children:[e.jsx(_,{className:"mr-2 h-4 w-4"}),"Notifications"]})]}),e.jsx(n,{}),e.jsxs(s,{onClick:m,children:[e.jsx(f,{className:"mr-2 h-4 w-4 text-red-600"}),e.jsx("span",{className:"text-red-600",children:"Log out"})]})]})]})})})}const O=Object.freeze(Object.defineProperty({__proto__:null,NavUser:z},Symbol.toStringTag,{value:"Module"}));export{_ as B,z as N,O as n};
